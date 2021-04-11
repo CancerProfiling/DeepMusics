ffcda4b9064169a6b9ea67491af8252c0c3f5e82
Lianhe Zhao <76764673+CancerProfiling@users.noreply.github.com>
2021-04-11 22:32:28 +0800

    Update DataLoader.py

--- DataLoader.py
+++ DataLoader.py
@@ -6,23 +6,26 @@ def sort_data(path):
 	
 	data = pd.read_csv(path)
 	data.sort_values("OS_MONTHS", ascending = False, inplace = True)
-	x = data.drop(["SAMPLE_ID", "OS_MONTHS", "OS_EVENT"], axis = 1).values
+	x = data.drop(["SAMPLE_ID", "OS_MONTHS", "OS_EVENT", "Clinical"], axis = 1).values
 	ytime = data.loc[:, ["OS_MONTHS"]].values
 	yevent = data.loc[:, ["OS_EVENT"]].values
-	return(x, ytime, yevent)
+	clinical = data.loc[:, ["Clinical"]].values
+
+	return(x, ytime, yevent,clinical)
 
 
 def load_data(path, dtype):
-	x, ytime, yevent = sort_data(path)
-
+	x, ytime, yevent,clinical = sort_data(path)
 	X = torch.from_numpy(x).type(dtype)
 	YTIME = torch.from_numpy(ytime).type(dtype)
 	YEVENT = torch.from_numpy(yevent).type(dtype)
+	CLINICAL = torch.from_numpy(clinical).type(dtype)
 	if torch.cuda.is_available():
 		X = X.cuda()
 		YTIME = YTIME.cuda()
 		YEVENT = YEVENT.cuda()
-	return(X, YTIME, YEVENT)
+		CLINICAL=CLINICAL.cuda()
+	return(X, YTIME, YEVENT,CLINICAL)
 
 
 def load_pathway(path, dtype):
