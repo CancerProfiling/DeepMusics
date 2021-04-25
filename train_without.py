d42d6b10655158ad2d2b4c6f3910d41385a61b55
Lianhe Zhao <76764673+CancerProfiling@users.noreply.github.com>
2021-04-25 23:03:52 +0800

    Update train_without.py

--- train_without.py
+++ train_without.py
@@ -1,6 +1,6 @@
 from DeepMusics import DeepMusicsNet
 from SubNetwork import dropout_mask, s_mask
-from Survival import R_set, neg_par_log_likelihood, c_index
+from Survival import R_set, neg_par_log_likelihood, 
 import torch
 import torch.optim as optim
 import copy
